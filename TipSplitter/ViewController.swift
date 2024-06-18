bc84a40c7ba5ec7f1189371115145f80d86c6dfa
Giovanna Moeller <giovannagmoeller@gmail.com>
2024-06-17 22:17:26 -0300

    Initial Commit

---
+++ TipSplitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipSplitter
+//
+//  Created by Giovanna Moeller on 17/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
