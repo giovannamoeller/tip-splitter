8628cbd03414fe395460d8d0f53a90877c8750bd
Giovanna Moeller <giovannagmoeller@gmail.com>
2024-06-17 22:54:16 -0300

    implementing base structure of each view

--- TipSplitter/Views/LogoView.swift
+++ TipSplitter/Views/LogoView.swift
@@ -8,13 +8,26 @@
 import UIKit
 
 class LogoView: UIView {
+    
+    private lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "logo-transparent"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    init() {
+        super.init(frame: .zero)
+        setupLayout()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupLayout() {
+        backgroundColor = .red
+        addSubview(logoImageView)
+    }
+    
 }
--- TipSplitter/Views/SplitInputView.swift
+++ TipSplitter/Views/SplitInputView.swift
@@ -9,12 +9,17 @@ import UIKit
 
 class SplitInputView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    init() {
+        super.init(frame: .zero)
+        setupLayout()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupLayout() {
+        backgroundColor = .purple
+    }
+    
 }
