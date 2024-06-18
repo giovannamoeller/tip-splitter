3a474cdd9bc467a26e6a206b5678071b451889ef
Giovanna Moeller <giovannagmoeller@gmail.com>
2024-06-18 16:50:15 -0300

    changing primaryText and secondaryText to topText and bottomText

--- TipSplitter/Views/Components/SplittedLabelView.swift
+++ TipSplitter/Views/Components/SplittedLabelView.swift
@@ -9,30 +9,30 @@ import UIKit
 
 class SplittedLabelView: UIView {
     
-    private var primaryText: String
-    private var secondaryText: String
+    private var topText: String
+    private var bottomText: String
     
-    private lazy var primaryTextLabel: UILabel = {
+    private lazy var topTextLabel: UILabel = {
         let label = UILabel()
-        label.text = primaryText
+        label.text = topText
         label.textColor = ThemeColor.textColor
         label.font = ThemeFont.bodyBold
         label.textAlignment = .left
         return label
     }()
     
-    private lazy var secondaryTextLabel: UILabel = {
+    private lazy var bottomTextLabel: UILabel = {
         let label = UILabel()
-        label.text = secondaryText
+        label.text = bottomText
         label.textColor = ThemeColor.textColor
         label.font = ThemeFont.bodyRegular
         label.textAlignment = .left
         return label
     }()
 
-    init(primaryText: String, secondaryText: String) {
-        self.primaryText = primaryText
-        self.secondaryText = secondaryText
+    init(topText: String, bottomText: String) {
+        self.topText = topText
+        self.bottomText = bottomText
         super.init(frame: .zero)
         addSubviews()
         setupConstraints()
@@ -43,19 +43,19 @@ class SplittedLabelView: UIView {
     }
     
     private func addSubviews() {
-        addSubview(primaryTextLabel)
-        addSubview(secondaryTextLabel)
+        addSubview(topTextLabel)
+        addSubview(bottomTextLabel)
     }
     
     private func setupConstraints() {
-        primaryTextLabel.snp.makeConstraints { constraintMaker in
+        topTextLabel.snp.makeConstraints { constraintMaker in
             constraintMaker.leading.equalToSuperview()
             constraintMaker.centerY.equalToSuperview().offset(-12.0)
         }
         
-        secondaryTextLabel.snp.makeConstraints { constraintMaker in
+        bottomTextLabel.snp.makeConstraints { constraintMaker in
             constraintMaker.leading.equalToSuperview()
-            constraintMaker.top.equalTo(primaryTextLabel.snp.bottom)
+            constraintMaker.top.equalTo(topTextLabel.snp.bottom)
         }
     }
     
