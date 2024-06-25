06b00fcd12d4be89e88be4a7ecc5e5da7f2d459d
Giovanna Moeller <giovannagmoeller@gmail.com>
2024-06-25 14:23:27 -0300

    adding snapshot tests to views (initial state)

--- TipSplitterTests/TipSplitterSnapshotTests.swift
+++ TipSplitterTests/TipSplitterSnapshotTests.swift
@@ -11,4 +11,42 @@ import SnapshotTesting
 
 final class TipSplitterSnapshotTests: XCTestCase {
     
+    private var screenWidth: CGFloat {
+        return UIScreen.main.bounds.size.width
+    }
+    
+    func testLogoView() {
+        // Given:
+        let size = CGSize(width: screenWidth, height: 64.0)
+        
+        // When:
+        let view = LogoView()
+        
+        // Then:
+        assertSnapshot(of: view, as: .image(size: size))
+    }
+    
+    func testInitialResultView() {
+        let size = CGSize(width: screenWidth, height: 220.0)
+        let view = TipResultView()
+        assertSnapshot(of: view, as: .image(size: size))
+    }
+    
+    func testInitialBillInputView() {
+        let size = CGSize(width: screenWidth, height: 60.0)
+        let view = BillInputView()
+        assertSnapshot(of: view, as: .image(size: size))
+    }
+    
+    func testInitialTipInputView() {
+        let size = CGSize(width: screenWidth, height: 120.0)
+        let view = TipInputView()
+        assertSnapshot(of: view, as: .image(size: size))
+    }
+    
+    func testInitialSplitInputView() {
+        let size = CGSize(width: screenWidth, height: 60.0)
+        let view = SplitInputView()
+        assertSnapshot(of: view, as: .image(size: size))
+    }
 }
